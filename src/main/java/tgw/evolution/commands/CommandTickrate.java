1bce22234b650b71c0e15a0fe14d87806d1cb003
TheGreatWolf <fagschultz@gmail.com>
2021-09-24 13:32:46 -0300

    Improved the tickrate command.

--- src/main/java/tgw/evolution/commands/CommandTickrate.java
+++ src/main/java/tgw/evolution/commands/CommandTickrate.java
@@ -15,17 +15,36 @@ public class CommandTickrate implements Command<CommandSource> {
     private static final FloatArgumentType TPS = FloatArgumentType.floatArg(TickrateChanger.MIN_TICKRATE, TickrateChanger.MAX_TICKRATE);
 
     public static void register(CommandDispatcher<CommandSource> dispatcher) {
-        dispatcher.register(Commands.literal("tickrate").requires(cs -> cs.hasPermission(3)).then(Commands.argument("tps", TPS).executes(CMD)));
+        dispatcher.register(Commands.literal("tickrate")
+                                    .requires(cs -> cs.hasPermission(3))
+                                    .executes(CMD)
+                                    .then(Commands.argument("tps", TPS).executes(CMD))
+                                    .then(Commands.literal("default").executes(CMD)));
     }
 
     @Override
     public int run(CommandContext<CommandSource> context) {
-        float tickrate = FloatArgumentType.getFloat(context, "tps");
+        String input = context.getInput();
+        CommandSource source = context.getSource();
+        float tickrate = Float.NaN;
+        switch (input) {
+            case "/tickrate": {
+                source.sendSuccess(new TranslationTextComponent("command.evolution.tickrate.current", TickrateChanger.getCurrentTickrate()), false);
+                return SINGLE_SUCCESS;
+            }
+            case "/tickrate default": {
+                tickrate = 20;
+                break;
+            }
+        }
+        if (Float.isNaN(tickrate)) {
+            tickrate = FloatArgumentType.getFloat(context, "tps");
+        }
         boolean change = TickrateChanger.updateServerTickrate(tickrate);
         if (!change) {
             return 0;
         }
-        context.getSource().sendSuccess(new TranslationTextComponent("command.evolution.tickrate", tickrate), true);
+        source.sendSuccess(new TranslationTextComponent("command.evolution.tickrate.change", tickrate), true);
         return SINGLE_SUCCESS;
     }
 }
