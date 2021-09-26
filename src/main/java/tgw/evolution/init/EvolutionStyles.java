0bc1ed315f2e566570352b8bcef5a72196a320d6
TheGreatWolf <fagschultz@gmail.com>
2021-09-26 09:21:09 -0300

    Changed the color of the durability tooltip from white to light grey.

--- src/main/java/tgw/evolution/init/EvolutionStyles.java
+++ src/main/java/tgw/evolution/init/EvolutionStyles.java
@@ -12,10 +12,10 @@ public final class EvolutionStyles {
     public static final Style MINING = Style.EMPTY.applyFormat(TextFormatting.AQUA);
     public static final Style REACH = Style.EMPTY.applyFormat(TextFormatting.YELLOW);
     public static final Style WHITE = Style.EMPTY.applyFormat(TextFormatting.WHITE);
-    public static final Style DURABILITY = WHITE;
     public static final Style INFO = Style.EMPTY.applyFormat(TextFormatting.BLUE);
     public static final Style PROPERTY = Style.EMPTY.applyFormat(TextFormatting.GOLD);
     public static final Style LIGHT_GREY = Style.EMPTY.applyFormat(TextFormatting.GRAY);
+    public static final Style DURABILITY = LIGHT_GREY;
     public static final Style MASS = Style.EMPTY.applyFormat(TextFormatting.DARK_GREEN);
     public static final Style LORE = Style.EMPTY.applyFormat(TextFormatting.LIGHT_PURPLE).withItalic(true);
     public static final Style EFFECTS = Style.EMPTY.applyFormat(TextFormatting.DARK_AQUA);
