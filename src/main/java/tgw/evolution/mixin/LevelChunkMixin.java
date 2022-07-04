2ea47921b3dd16f317e9bf83c20774d07fba84c4
TheGreatWolf <fagschultz@gmail.com>
2022-07-04 20:54:53 -0300

    Improvements to lighting performance.

--- src/main/java/tgw/evolution/mixin/LevelChunkMixin.java
+++ src/main/java/tgw/evolution/mixin/LevelChunkMixin.java
@@ -24,6 +24,9 @@ import org.spongepowered.asm.mixin.Shadow;
 import tgw.evolution.blocks.IFluidLoggable;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Mixin(LevelChunk.class)
 public abstract class LevelChunkMixin extends ChunkAccess {
@@ -74,4 +77,41 @@ public abstract class LevelChunkMixin extends ChunkAccess {
 
     @Shadow
     public abstract Level getLevel();
+
+    /**
+     * This implementation avoids iterating over empty chunk sections and uses direct access to read out block states
+     * instead. Instead of allocating a BlockPos for every block in the chunk, they're now only allocated once we find
+     * a light source.
+     *
+     * @reason Use optimized implementation
+     * @author JellySquid
+     */
+    @Override
+    @Overwrite
+    public Stream<BlockPos> getLights() {
+        List<BlockPos> list = new ArrayList<>();
+        int minX = this.chunkPos.getMinBlockX();
+        int minZ = this.chunkPos.getMinBlockZ();
+        for (LevelChunkSection section : this.sections) {
+            if (section == null || section.hasOnlyAir()) {
+                continue;
+            }
+            int startY = section.bottomBlockY();
+            for (int x = 0; x < 16; x++) {
+                for (int y = 0; y < 16; y++) {
+                    for (int z = 0; z < 16; z++) {
+                        BlockState state = section.getBlockState(x, y, z);
+                        if (state.getLightEmission() != 0) {
+                            //noinspection ObjectAllocationInLoop
+                            list.add(new BlockPos(minX + x, startY + y, minZ + z));
+                        }
+                    }
+                }
+            }
+        }
+        if (list.isEmpty()) {
+            return Stream.empty();
+        }
+        return list.stream();
+    }
 }
