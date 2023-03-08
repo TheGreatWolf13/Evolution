2d9c56d6f0b86c61bbd53dcf5a6702b21c921613
TheGreatWolf <fagschultz@gmail.com>
2023-03-07 22:30:29 -0300

    Cleanup.

--- src/main/java/tgw/evolution/util/time/Time.java
+++ src/main/java/tgw/evolution/util/time/Time.java
@@ -7,68 +7,46 @@ import tgw.evolution.util.math.Metric;
 public class Time {
     public static final Time START_TIME = new Time(6);
     public static final int STARTING_YEAR = 1_000;
-    public static final int TICKS_PER_HOUR;
     public static final int TICKS_PER_DAY = 36_000;
-    public static final int TICKS_PER_MONTH;
-    public static final int TICKS_PER_YEAR;
     public static final int DAYS_PER_MONTH = 21;
-    public static final int DAYS_PER_YEAR;
-    public static final int MONTHS_PER_YEAR;
+    public static final int MONTHS_PER_YEAR = 12;
+    public static final int TICKS_PER_HOUR = TICKS_PER_DAY / 24;
+    public static final int TICKS_PER_MONTH = DAYS_PER_MONTH * TICKS_PER_DAY;
+    public static final int DAYS_PER_YEAR = MONTHS_PER_YEAR * DAYS_PER_MONTH;
+    public static final int TICKS_PER_YEAR = DAYS_PER_YEAR * TICKS_PER_DAY;
     /**
      * How long it takes for Mercury to orbit the Sun. In real life, this value is 87.9691 days, which is then multiplied by
      * {@link Time#DAYS_PER_YEAR}
      * and divided by 365.25 (a real life, Earth year).
      */
-    public static final int MERCURIAN_YEAR;
+    public static final int MERCURIAN_YEAR = (int) (87.969_1 * DAYS_PER_YEAR / 365.25 * TICKS_PER_DAY);
     /**
      * How long it takes for Venus to orbit the Sun. In real life, this value is 224.7 days, which is then multiplied by {@link Time#DAYS_PER_YEAR}
      * and divided by 365.25 (a real life, Earth year).
      */
-    public static final int VENUSIAN_YEAR;
+    public static final int VENUSIAN_YEAR = (int) (224.7 * DAYS_PER_YEAR / 365.25 * TICKS_PER_DAY);
     /**
      * How long it takes for Mars to orbit the Sun. In real life, this value is 686.971 days, which is then multiplied by {@link Time#DAYS_PER_YEAR}
      * and divided by 365.25 (a real life, Earth year).
      */
-    public static final int MARTIAN_YEAR;
+    public static final int MARTIAN_YEAR = (int) (686.971 * DAYS_PER_YEAR / 365.25 * TICKS_PER_DAY);
     /**
      * How long it takes for Jupiter to orbit the Sun. In real life, this value is 4_331.572 days, which is then multiplied by
      * {@link Time#DAYS_PER_YEAR}
      * and divided by 365.25 (a real life, Earth year).
      */
-    public static final int JUPITERIAN_YEAR;
+    public static final int JUPITERIAN_YEAR = (int) (4_331.572 * DAYS_PER_YEAR / 365.25 * TICKS_PER_DAY);
     /**
      * How long it takes for Saturn to orbit the Sun. In real life, this value is 10_759.22 days, which is then multiplied by
      * {@link Time#DAYS_PER_YEAR}
      * and divided by 365.25 (a real life, Earth year).
      */
-    public static final int SATURNIAN_YEAR;
+    public static final int SATURNIAN_YEAR = (int) (10_759.22 * DAYS_PER_YEAR / 365.25 * TICKS_PER_DAY);
     /**
      * A sidereal day presents the time it takes for the Earth to spin around its axis by 360º relative to the background stars.
      * It is equivalent to 23h 56min 04s.
      */
-    public static final int SIDEREAL_DAY_IN_TICKS;
-
-    static {
-        //noinspection ConstantConditions
-        if (TICKS_PER_DAY % 24 != 0) {
-            throw new RuntimeException("Day should have a multiple of 24 number of ticks!");
-        }
-        TICKS_PER_HOUR = TICKS_PER_DAY / 24;
-        //noinspection ConstantConditions
-        if (DAYS_PER_MONTH % 7 != 0) {
-            throw new RuntimeException("Month should have a multiple of 7 number of days!");
-        }
-        TICKS_PER_MONTH = DAYS_PER_MONTH * TICKS_PER_DAY;
-        MONTHS_PER_YEAR = 12;
-        DAYS_PER_YEAR = MONTHS_PER_YEAR * DAYS_PER_MONTH;
-        TICKS_PER_YEAR = DAYS_PER_YEAR * TICKS_PER_DAY;
-        MERCURIAN_YEAR = (int) (87.969_1 * DAYS_PER_YEAR / 365.25 * TICKS_PER_DAY);
-        VENUSIAN_YEAR = (int) (224.7 * DAYS_PER_YEAR / 365.25 * TICKS_PER_DAY);
-        MARTIAN_YEAR = (int) (686.971 * DAYS_PER_YEAR / 365.25 * TICKS_PER_DAY);
-        JUPITERIAN_YEAR = (int) (4_331.572 * DAYS_PER_YEAR / 365.25 * TICKS_PER_DAY);
-        SATURNIAN_YEAR = (int) (10_759.22 * DAYS_PER_YEAR / 365.25 * TICKS_PER_DAY);
-        SIDEREAL_DAY_IN_TICKS = (int) (TICKS_PER_DAY * (23 + 56 / 60.0 + 4 / (60.0 * 60.0)) / 24.0);
-    }
+    public static final int SIDEREAL_DAY_IN_TICKS = (int) (TICKS_PER_DAY * (23 + 56 / 60.0 + 4 / (60.0 * 60.0)) / 24.0);
 
     private final int hour;
     private final int minute;
