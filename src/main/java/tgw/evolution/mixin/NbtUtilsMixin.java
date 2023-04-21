88e227957c0b5a1a34e3131b6e4f04b30d026850
TheGreatWolf <fagschultz@gmail.com>
2023-04-21 20:00:58 -0300

    Fixed NPE caused by the removed DataFixers.

---
+++ src/main/java/tgw/evolution/mixin/NbtUtilsMixin.java
@@ -0,0 +1,30 @@
+package tgw.evolution.mixin;
+
+import com.mojang.datafixers.DataFixer;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.util.datafix.DataFixTypes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(NbtUtils.class)
+public abstract class NbtUtilsMixin {
+
+    /**
+     * @author TheGreatWolf
+     * @reason Remove DataFixers references, as they are disabled.
+     */
+    @Overwrite
+    public static CompoundTag update(DataFixer dataFixer, DataFixTypes type, CompoundTag tag, int version, int newVersion) {
+        return tag;
+    }
+
+    /**
+     * @author TheGreatWolf
+     * @reason Remove DataFixers references, as they are disabled.
+     */
+    @Overwrite
+    public static CompoundTag update(DataFixer dataFixer, DataFixTypes type, CompoundTag tag, int version) {
+        return tag;
+    }
+}
