f2c21aa9d4b5a411a4a75af2b4f45d77693097c5
TheGreatWolf <fagschultz@gmail.com>
2023-04-22 05:47:05 -0300

    Documented common values from friction coefficient.

--- src/main/java/tgw/evolution/patches/IBlockPatch.java
+++ src/main/java/tgw/evolution/patches/IBlockPatch.java
@@ -10,6 +10,16 @@ import tgw.evolution.util.constants.HarvestLevel;
 
 public interface IBlockPatch {
 
+    /**
+     * Common Values: <br>
+     * 0.55 = Sand; <br>
+     * 0.57 = Gravel; <br>
+     * 0.60 = Clay, Glass, Grass; <br>
+     * 0.61 = Dry Grass; <br>
+     * 0.63 = Dirt; <br>
+     * 0.70 = Wood; <br>
+     * 0.80 = Stone; <br>
+     */
     float getFrictionCoefficient(BlockState state);
 
     @HarvestLevel
