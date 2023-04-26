433da531ab883b2aec1747fcd7be20536de64eeb
TheGreatWolf <fagschultz@gmail.com>
2023-04-26 01:25:23 -0300

    Removed iterator allocation from RecipeBookComponent.

---
+++ src/main/java/tgw/evolution/mixin/RecipeBookComponentMixin.java
@@ -0,0 +1,30 @@
+package tgw.evolution.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
+import net.minecraft.world.item.crafting.Recipe;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
+
+@Mixin(RecipeBookComponent.class)
+public abstract class RecipeBookComponentMixin extends GuiComponent {
+
+    @Shadow
+    protected Minecraft minecraft;
+
+    /**
+     * @author TheGreatWolf
+     * @reason Avoid iterator allocation
+     */
+    @Overwrite
+    public void recipesShown(List<Recipe<?>> recipes) {
+        assert this.minecraft.player != null;
+        for (int i = 0, len = recipes.size(); i < len; i++) {
+            this.minecraft.player.removeRecipeHighlight(recipes.get(i));
+        }
+    }
+}
