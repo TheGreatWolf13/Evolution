ad3164a081a38378c66d699fab072a1b3c2e61a8
TheGreatWolf <fagschultz@gmail.com>
2023-06-08 17:18:10 -0300

    Cleanup.

--- src/main/java/tgw/evolution/patches/IMinecraftServerPatch.java
+++ src/main/java/tgw/evolution/patches/IMinecraftServerPatch.java
@@ -1,12 +1,6 @@
 package tgw.evolution.patches;
 
-import it.unimi.dsi.fastutil.objects.Object2BooleanMap;
-
-import java.util.UUID;
-
 public interface IMinecraftServerPatch {
-
-    Object2BooleanMap<UUID> getMultiplayerPauseSentPackets();
 
     boolean isMultiplayerPaused();
 
