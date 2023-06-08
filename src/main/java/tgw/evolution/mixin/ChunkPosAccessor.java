6c5146104c532a5fa4666908a886ce61b4bec6e3
TheGreatWolf <fagschultz@gmail.com>
2023-06-08 16:25:48 -0300

    Fixed mixin accessor not able to mutate final fields.

--- src/main/java/tgw/evolution/mixin/ChunkPosAccessor.java
+++ src/main/java/tgw/evolution/mixin/ChunkPosAccessor.java
@@ -2,14 +2,17 @@ package tgw.evolution.mixin;
 
 import net.minecraft.world.level.ChunkPos;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(ChunkPos.class)
 public interface ChunkPosAccessor {
 
+    @Mutable
     @Accessor(value = "x")
     void setX(int x);
 
+    @Mutable
     @Accessor(value = "z")
     void setZ(int z);
 }
