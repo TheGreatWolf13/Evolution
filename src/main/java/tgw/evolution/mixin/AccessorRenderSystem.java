f6017db7901ebfff2c961956698a3b49d8acf636
TheGreatWolf <fagschultz@gmail.com>
2023-08-26 16:49:21 -0300

    Cleanup.

--- src/main/java/tgw/evolution/mixin/AccessorRenderSystem.java
+++ src/main/java/tgw/evolution/mixin/AccessorRenderSystem.java
@@ -2,6 +2,7 @@ package tgw.evolution.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.renderer.ShaderInstance;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
@@ -12,8 +13,10 @@ public interface AccessorRenderSystem {
     /**
      * Sets the shader directly, without the need for a supplier. This should be only used when we are guaranteed to be on the main thread.
      */
+    @Contract(value = "_ -> _")
     @Accessor
     static void setShader(@Nullable ShaderInstance shader) {
+        //noinspection Contract
         throw new AbstractMethodError();
     }
 }
