13f44726f4830dafa445669d75f9470f43fbeb56
TheGreatWolf <fagschultz@gmail.com>
2023-12-02 16:18:37 -0300

    Cleanup.

--- src/main/java/tgw/evolution/client/renderer/ambient/LightTextureEv.java
+++ src/main/java/tgw/evolution/client/renderer/ambient/LightTextureEv.java
@@ -2,7 +2,6 @@ package tgw.evolution.client.renderer.ambient;
 
 import com.mojang.blaze3d.platform.NativeImage;
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.GameRenderer;
@@ -17,14 +16,6 @@ import tgw.evolution.events.ClientEvents;
 
 public class LightTextureEv extends LightTexture {
 
-    private static final Vector3f LERP_0 = new Vector3f(1.0F, 1.0F, 1.0F);
-    private static final Vector3f LERP_1 = new Vector3f(0.99F, 1.12F, 1.0F);
-    private static final Vector3f VEC_0 = new Vector3f();
-    private static final Vector3f VEC_1 = new Vector3f();
-    private static final Vector3f VEC_2 = new Vector3f();
-    private static final Vector3f VEC_3 = new Vector3f();
-    private static final Vector3f VEC_4 = new Vector3f();
-    private static final Vector3f VEC_5 = new Vector3f();
     private final GameRenderer gameRenderer;
     private final NativeImage lightPixels;
     private final DynamicTexture lightTexture;
@@ -73,10 +64,6 @@ public class LightTextureEv extends LightTexture {
         return 1.0F - f * f * f * f;
     }
 
-    private static void setVec(Vector3f ref, Vector3f assign) {
-        ref.set(assign.x(), assign.y(), assign.z());
-    }
-
     @Override
     public void close() {
         this.lightTexture.close();
@@ -120,65 +107,110 @@ public class LightTextureEv extends LightTexture {
                 }
                 assert this.mc.player != null;
                 float waterBrightness = this.mc.player.getWaterVision();
-                float nightVisionModifier;
+                float nightVisionMod;
                 if (this.mc.player.hasEffect(MobEffects.NIGHT_VISION)) {
-                    nightVisionModifier = GameRenderer.getNightVisionScale(this.mc.player, partialTicks);
+                    nightVisionMod = GameRenderer.getNightVisionScale(this.mc.player, partialTicks);
                 }
                 else if (waterBrightness > 0.0F && this.mc.player.hasEffect(MobEffects.CONDUIT_POWER)) {
-                    nightVisionModifier = waterBrightness;
+                    nightVisionMod = waterBrightness;
                 }
                 else {
-                    nightVisionModifier = 0.0F;
+                    nightVisionMod = 0.0F;
                 }
-                VEC_0.set(skyBrightness, skyBrightness, skyBrightness);
-                VEC_0.lerp(LERP_0, 0.35F);
-                float f4 = this.torchFlicker + 1.5F;
-                for (int y = 0; y < 16; y++) {
-                    for (int x = 0; x < 16; x++) {
-                        float skyLight = getLightBrightness(level, y) * skyFlash;
-                        float bLRed = getLightBrightness(level, x) * f4;
+                float corrSkyBrightness = skyBrightness * 0.65f + 0.35f;
+                float flicker = this.torchFlicker + 1.5F;
+                for (int sl = 0; sl < 16; sl++) {
+                    for (int bl = 0; bl < 16; bl++) {
+                        float skyLight = getLightBrightness(level, sl) * skyFlash;
+                        float bLRed = getLightBrightness(level, bl) * flicker;
                         float bLGreen = bLRed * ((bLRed * 0.6F + 0.4F) * 0.6F + 0.4F);
                         float bLBlue = bLRed * (bLRed * bLRed * 0.6F + 0.4F);
-                        VEC_1.set(bLRed, bLGreen, bLBlue);
                         if (level.effects().forceBrightLightmap()) {
-                            VEC_1.lerp(LERP_1, 0.25F);
+                            bLRed *= 0.75f;
+                            bLGreen *= 0.75f;
+                            bLBlue *= 0.75f;
+                            bLRed += 0.99f * 0.25f;
+                            bLGreen += 1.12f * 0.25f;
+                            bLBlue += 0.25f;
                         }
                         else {
-                            setVec(VEC_2, VEC_0);
-                            VEC_2.mul(skyLight);
-                            VEC_1.add(VEC_2);
+                            float addend = corrSkyBrightness * skyLight;
+                            bLRed += addend;
+                            bLGreen += addend;
+                            bLBlue += addend;
                             if (this.gameRenderer.getDarkenWorldAmount(partialTicks) > 0.0F) {
-                                float f9 = this.gameRenderer.getDarkenWorldAmount(partialTicks);
-                                setVec(VEC_3, VEC_1);
-                                VEC_3.mul(0.7F, 0.6F, 0.6F);
-                                VEC_1.lerp(VEC_3, f9);
+                                float darkenAmount = this.gameRenderer.getDarkenWorldAmount(partialTicks);
+                                float r = bLRed * 0.7f * darkenAmount;
+                                float g = bLGreen * 0.6f * darkenAmount;
+                                float b = bLBlue * 0.6f * darkenAmount;
+                                float f = 1.0f - darkenAmount;
+                                bLRed *= f;
+                                bLGreen *= f;
+                                bLBlue *= f;
+                                bLRed += r;
+                                bLGreen += g;
+                                bLBlue += b;
                             }
                         }
-                        VEC_1.clamp(0.0F, 1.0F);
-                        if (nightVisionModifier > 0.0F) {
-                            float f10 = Math.max(VEC_1.x(), Math.max(VEC_1.y(), VEC_1.z()));
-                            if (f10 < 1.0F) {
-                                setVec(VEC_5, VEC_1);
-                                float f12 = 1.0F / f10;
-                                if (Float.isInfinite(f12)) {
-                                    VEC_5.set(1.0f, 1.0f, 1.0f);
+                        if (bLRed > 1) {
+                            bLRed = 1;
+                        }
+                        assert bLRed >= 0;
+                        if (bLGreen > 1) {
+                            bLGreen = 1;
+                        }
+                        assert bLGreen >= 0;
+                        if (bLBlue > 1) {
+                            bLBlue = 1;
+                        }
+                        assert bLBlue >= 0;
+                        if (nightVisionMod > 0.0F) {
+                            float max = Math.max(bLRed, Math.max(bLGreen, bLBlue));
+                            if (max < 1.0F) {
+                                float r = bLRed;
+                                float g = bLGreen;
+                                float b = bLBlue;
+                                float f = 1.0F / max;
+                                if (Float.isInfinite(f)) {
+                                    r = 1.0f;
+                                    g = 1.0f;
+                                    b = 1.0f;
                                 }
                                 else {
-                                    VEC_5.mul(f12);
+                                    r *= f;
+                                    g *= f;
+                                    b *= f;
                                 }
-                                VEC_1.lerp(VEC_5, nightVisionModifier);
+                                float m = 1 - nightVisionMod;
+                                bLRed *= m;
+                                bLGreen *= m;
+                                bLBlue *= m;
+                                bLRed += r * nightVisionMod;
+                                bLGreen += g * nightVisionMod;
+                                bLBlue += b * nightVisionMod;
                             }
                         }
                         float gamma = (float) this.mc.options.gamma;
-                        setVec(VEC_4, VEC_1);
-                        VEC_4.map(LightTextureEv::invGamma);
-                        VEC_1.lerp(VEC_4, gamma);
-                        VEC_1.clamp(0.0F, 1.0F);
-                        VEC_1.mul(255.0F);
-                        int red = (int) VEC_1.x();
-                        int green = (int) VEC_1.y();
-                        int blue = (int) VEC_1.z();
-                        this.lightPixels.setPixelRGBA(x, y, 0xff00_0000 | blue << 16 | green << 8 | red);
+                        float r = invGamma(bLRed) * gamma;
+                        float g = invGamma(bLGreen) * gamma;
+                        float b = invGamma(bLBlue) * gamma;
+                        float f = 1.0f - gamma;
+                        bLRed *= f;
+                        bLGreen *= f;
+                        bLBlue *= f;
+                        bLRed += r;
+                        bLGreen += g;
+                        bLBlue += b;
+                        assert 0 <= bLRed && bLRed <= 1;
+                        assert 0 <= bLGreen && bLGreen <= 1;
+                        assert 0 <= bLBlue && bLBlue <= 1;
+                        bLRed *= 255.0f;
+                        bLGreen *= 255.0f;
+                        bLBlue *= 255.0f;
+                        int red = (int) bLRed;
+                        int green = (int) bLGreen;
+                        int blue = (int) bLBlue;
+                        this.lightPixels.setPixelRGBA(bl, sl, 0xff00_0000 | blue << 16 | green << 8 | red);
                     }
                 }
                 this.lightTexture.upload();
