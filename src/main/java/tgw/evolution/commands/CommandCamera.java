d0cb3a8bf66980d3223113252b3f6ba19c2a73ff
TheGreatWolf <fagschultz@gmail.com>
2024-03-16 00:14:31 -0300

    Removed ambiguity on /camera.

--- src/main/java/tgw/evolution/commands/CommandCamera.java
+++ src/main/java/tgw/evolution/commands/CommandCamera.java
@@ -1,43 +1,39 @@
 package tgw.evolution.commands;
 
-import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 
-public final class CommandCamera implements Command<CommandSourceStack> {
+public final class CommandCamera {
 
-    private static final Command<CommandSourceStack> CMD = new CommandCamera();
+    private CommandCamera() {
+    }
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("camera")
                                     .requires(cs -> cs.getEntity() instanceof Player && cs.hasPermission(2))
                                     .then(Commands.literal("reset")
-                                                  .executes(CMD)
+                                                  .executes(CommandCamera::reset)
                                     )
-                                    .then(Commands.argument("target", EntityArgument.entity())
-                                                  .executes(CMD)
+                                    .then(Commands.literal("set")
+                                                  .then(Commands.argument("target", EntityArgument.entity())
+                                                                .executes(CommandCamera::set)
+                                                  )
                                     )
         );
     }
 
-    @Override
-    public int run(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
-        String input = context.getInput();
-        CommandSourceStack source = context.getSource();
-        ServerPlayer player = source.getPlayerOrException();
-        if ("/camera reset".equals(input)) {
-            player.setCamera(null);
-            return SINGLE_SUCCESS;
-        }
-        Entity entity = EntityArgument.getEntity(context, "target");
-        player.setCamera(entity);
-        return SINGLE_SUCCESS;
+    private static int reset(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        context.getSource().getPlayerOrException().setCamera(null);
+        return 1;
+    }
+
+    private static int set(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        context.getSource().getPlayerOrException().setCamera(EntityArgument.getEntity(context, "target"));
+        return 1;
     }
 }
