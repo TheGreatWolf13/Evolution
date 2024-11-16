6207797c7b90d800cd1c2f955dbce19f9cdab625
TheGreatWolf <fagschultz@gmail.com>
2024-11-15 21:27:00 -0300

    Added more method options to PatchLevelAccessor.

--- src/main/java/tgw/evolution/patches/PatchLevelAccessor.java
+++ src/main/java/tgw/evolution/patches/PatchLevelAccessor.java
@@ -1,5 +1,6 @@
 package tgw.evolution.patches;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.Block;
@@ -12,15 +13,25 @@ public interface PatchLevelAccessor {
         throw new AbstractMethodError();
     }
 
+    @SuppressWarnings("unused")
     default DifficultyInstance getCurrentDifficultyAt_(int x, int y, int z) {
         throw new AbstractMethodError();
     }
 
+    @SuppressWarnings("unused")
     default void levelEvent_(@Nullable Player player, @LvlEvent int event, int x, int y, int z, int data) {
         throw new AbstractMethodError();
     }
 
+    default void levelEvent_(@Nullable Player player, @LvlEvent int event, BlockPos pos, int data) {
+        this.levelEvent_(player, event, pos.getX(), pos.getY(), pos.getZ(), data);
+    }
+
     default void levelEvent_(@LvlEvent int event, int x, int y, int z, int data) {
         this.levelEvent_(null, event, x, y, z, data);
+    }
+
+    default void levelEvent_(@LvlEvent int event, BlockPos pos, int data) {
+        this.levelEvent_(null, event, pos.getX(), pos.getY(), pos.getZ(), data);
     }
 }
